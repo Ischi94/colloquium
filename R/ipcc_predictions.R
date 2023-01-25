c490eda242220df1645117eb7dba97a10985626d
Ischi94 <gmathes94@gmx.de>
2023-01-25 12:37:37 +0100

    save ipcc data

--- R/ipcc_predictions.R
+++ R/ipcc_predictions.R
@@ -2,6 +2,30 @@ library(tidyverse)
 library(here)
 
 
+# load raw data
+dat_ipcc <- tibble(filename = list.files(here("data",
+                                              "raw",
+                                              "ipcc")) %>%
+                     str_remove("tas_global_") %>%
+                     str_remove(".csv")) %>% 
+  mutate(file_contents = map(filename,  
+                             ~ read_csv(list.files(here("data",
+                                                        "raw",
+                                                        "ipcc"),
+                                                   full.names = TRUE)))) %>% 
+  unnest(file_contents)
+  
+
+# save that joined data
+write_rds(dat_ipcc, here("data", 
+                         "ipcc_data.rds"))
+
+list.files(here("data", 
+                "raw", 
+                "ipcc"), 
+           full.names = TRUE) %>% 
+  map(read_csv)
+
 # save plot
 ggsave(plot_tax, filename = here("figures",
                                  "effect_taxonomy.png"), 
