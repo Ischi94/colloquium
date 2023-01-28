062c89f8d375ea3c797ed4e86bb601888a366d5e
Ischi94 <gmathes94@gmx.de>
2023-01-28 17:58:29 +0100

    correct label colour

--- R/origination_probability.R
+++ R/origination_probability.R
@@ -14,7 +14,8 @@ av <- median(prob$ori.prob)
 plot_ori <- ggplot(prob, aes(x = pal.int,
                  y = ori.prob,
                  fill = pal.int)) + 
-  geom_hline(yintercept = av) +
+  geom_hline(yintercept = av, 
+             colour = colour_yellow) +
   geom_violin() +
   scale_fill_manual(values = c(colour_blue, colour_blue, 
                                colour_red, colour_red))+
@@ -31,29 +32,32 @@ plot_ori <- ggplot(prob, aes(x = pal.int,
   # add grey lines to visualise medians per group
   stat_summary(fun = median, fun.min = median, fun.max = median,
                geom = "crossbar", width = c(0.915, 0.35, 0.53, 0.42),
-               colour = "grey55") +
+               colour = "grey60") +
   # add outer layer
-  geom_violin(fill = NA) +
+  geom_violin(fill = NA, 
+              colour = "grey40") +
   # add arrows
   annotate(geom = "curve", x = 4, y = 0.22,  # overall
            xend = 4.45, yend = 0.138,
            curvature = -.325, arrow = arrow(length = unit(2.5, "mm")),
-           colour = "grey40") +
+           colour = "grey60") +
   annotate(geom = "curve", x = 1.25, y = 0.055, # per group
            xend = 0.68, yend = 0.146,
            curvature = -.5, arrow = arrow(length = unit(2.5, "mm")),
-           colour = "grey40") +
+           colour = "grey60") +
   # add lines
   annotate(geom = "segment", x = c(4, 1.25), y = c(0.22, 0.055), # overall
-           xend = c(4.55, 0.575), yend = c(0.22, 0.055), colour = "grey40") +
+           xend = c(4.55, 0.575), yend = c(0.22, 0.055), colour = "grey60") +
+  # annotate(geom = "segment", x = c(4), y = c(0.22), # overall
+  #          xend = c(4.55), yend = c(0.22), colour = "grey40") +
   # add background box
   annotate(geom = "rect", xmin = 4, xmax = 4.5, # overall
            ymin = 0.235, ymax = 0.26, fill = "white") +
   # add text
   annotate(geom = "text", x = 4.275, y = c(0.25, 0.235), # overall
-           colour = "grey30", label = c("Overall", "median"), size = 14/.pt) +
+           colour = "grey40", label = c("Overall", "median"), size = 15/.pt) +
   annotate(geom = "text", x = 0.9, y = c(0.045, 0.03, 0.015), # per group
-           colour = "grey30", label = c("Median", "per", "group"), size = 3.5) +
+           colour = "grey40", label = c("Median", "per", "group"), size = 15/.pt) +
   scale_x_discrete(labels = c("Cooling-Cooling", "Cooling-Warming",
                               "Warming-Cooling", "Warming-Warming"),
                    guide = guide_axis(n.dodge = 2)) +
